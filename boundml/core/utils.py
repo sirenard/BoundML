28be3a5390db75abcd058836d607b2fab5eb9283
Simon Renard <renardsimon@skynet.be>
2026-01-09 11:11:03 +0100

    Allow FolderInstances to check problem readability using a child process before reading it

--- boundml/core/utils.py
+++ boundml/core/utils.py
@@ -1,6 +1,48 @@
+import os
+
+import pathos.multiprocessing as mp
+from os import fork
+
 import numpy as np
+from pyscipopt import Model
+
 
 def shifted_geometric_mean(values, shift=1.0):
     values = np.array(values)
     geom_mean = np.exp(np.mean(np.log(values + shift))) - shift
     return geom_mean
+
+def check_readability(path):
+    """
+    Check if a path is readable.
+    It creates a child process to start a new SCIP instance that will crash if path is not valid
+    """
+
+    def _check(path):
+        try:
+            # Create a dummy model just to test reading
+            m = Model()
+            m.setParam("display/verblevel", 0)
+            with open(os.devnull, 'w') as fnull:
+                os.dup2(fnull.fileno(), 2)
+                m.readProblem(path)
+            return True  # Success
+        except:
+            return False  # Caught a Python-level error
+        # If SCIP crashes (SegFault), this process dies, and the parent detects it.
+
+    # --- Safety Check Step ---
+    # We spawn a process to test the file.
+    with mp.Pool(processes=1) as pool:
+        # Run the check asynchronously
+        result = pool.apply_async(_check, (path,))
+
+        try:
+            # Wait up to 2 seconds for the read to finish.
+            # If the worker crashes (segfault), this raises an error or returns False.
+            is_safe = result.get(timeout=2)
+        except Exception:
+            is_safe = False
+
+        return is_safe
+
--- boundml/instances/folder_instances.py
+++ boundml/instances/folder_instances.py
@@ -1,9 +1,11 @@
 import os
 import random
+import warnings
 from typing import Callable
 
 from pyscipopt import Model
 
+from boundml.core.utils import check_readability
 from boundml.instances import Instances
 
 
@@ -16,7 +18,7 @@ class FolderInstances(Instances):
         "benchmark": "https://miplib.zib.de/downloads/benchmark.zip"
     }
 
-    def __init__(self, folder: str, filter: Callable[[str], bool] = lambda x: True):
+    def __init__(self, folder: str, filter: Callable[[str], bool] = lambda x: True, allow_reading_error=True):
         """
         Parameters
         ----------
@@ -24,22 +26,35 @@ class FolderInstances(Instances):
             Name of the folder containing the instances.
         filter : Callable[[str], bool]
             Filter instances based on their name to work only with a desired subset of instances
+        allow_reading_error: bool
+            If True and SCIP failed to read a problem, catch the error and go to the next one.
         """
         self._instances_dir = folder
 
         self._instances_files = [name for name in os.listdir(self._instances_dir) if filter(name)]
         self._instances_files.sort()
         self._index = 0
+        self.allow_reading_error = allow_reading_error
 
     def __next__(self):
         if self._index >= len(self._instances_files):
             raise StopIteration
 
         path = os.path.join(self._instances_dir, self._instances_files[self._index])
+        self._index += 1
         model = Model()
         model.setParam("display/verblevel", 0)
+
+        if self.allow_reading_error:
+            is_valid = check_readability(path)
+
+            if not is_valid:
+                warnings.warn(
+                    f"Failed to read problem {path}." + "Skipping to the next one." if self.allow_reading_error else "")
+                return next(self)
+
         model.readProblem(path)
-        self._index += 1
+
         return model
 
     def seed(self, seed: int):
